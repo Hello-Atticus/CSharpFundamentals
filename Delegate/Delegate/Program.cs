169d95641ab58f8f8066afd6c5a69e48847bf2f0
Atticus Finch <nineday9527@outlook.com>
2024-09-10 09:50:29 +1200

    Delegate:Action

--- Delegate/Delegate/Program.cs
+++ Delegate/Delegate/Program.cs
@@ -1,35 +1,12 @@
 ﻿namespace Delegate
 {
-    public delegate double Calc(double x, double y);
-
     internal class Program
     {
         static void Main(string[] args)
         {
-            Calculator calculator = new Calculator();
-            //calculator.Report();
-            Action action = new Action(calculator.Report);
-            action.Invoke();
-
-            Func<int, int, int> funcAdd = new Func<int, int, int>(calculator.Add);
-            Func<int, int, int> funcSub = new Func<int, int, int>(calculator.Sub);
-
-            int a = 100;
-            int b = 200;
-            int addResult = 0;
-            addResult = funcAdd.Invoke(a, b);
-            Console.WriteLine(addResult);
-
-            int subResult = 0;
-            subResult = funcSub.Invoke(a, b);
-            Console.WriteLine(subResult);
-
-            CalculatorDelegate calculatorDelegate = new CalculatorDelegate();
-            Calc calcAdd = new Calc(calculatorDelegate.Add);
-            double x = 15;
-            double y = 50;
-            double z = calcAdd.Invoke(x, y);
-            Console.WriteLine(z);
+            Calculator calc = new Calculator();
+            Action action = new Action(calc.Report);
+            action.Invoke();//it also can be action();
         }
     }
 
@@ -37,34 +14,20 @@
     {
         public void Report()
         {
-            Console.WriteLine("I have 3 methods!");
+            Console.WriteLine("I have a calculator.");
         }
-
-        public int Add(int a, int b)
+        public int Add(int x, int y)
         {
-            int result = a + b;
+            int result = x + y;
             return result;
         }
 
-        public int Sub(int a, int b)
+        public int Sub(int x, int y)
         {
-            int result = a - b;
+            int result = x - y;
             return result;
+        }
 
-        }
-    }
 
-    class CalculatorDelegate
-    {
-        public double Add(double a, double b)
-        {
-            double result = a + b;
-            return result;
-        }
-        public double Sub(double a, double b)
-        {
-            double result = a - b;
-            return result;
-        }
     }
 }
