772fc4372f9c347f576e9d88065fecbe828080c8
Atticus Finch <nineday9527@outlook.com>
2024-09-10 18:00:59 +1200

    Delegate: callback

--- DelegateExample/DelegateExample/Program.cs
+++ DelegateExample/DelegateExample/Program.cs
@@ -9,18 +9,30 @@
 
             Func<Product> func1 = new Func<Product>(productFactory.MakePizza);
             Func<Product> func2 = new Func<Product>(productFactory.MakeToyCar);
-            Box box1 = wrapFactory.WrapProduct(func1);
+
+            Logger logger = new Logger();
+            Action<Product> log = new Action<Product>(logger.Log);
+            Box box1 = wrapFactory.WrapProduct(func1,log);
             Console.WriteLine(box1.Product.Name);
 
-            Box box2 = wrapFactory.WrapProduct(func2);
+            Box box2 = wrapFactory.WrapProduct(func2,log);
             Console.WriteLine(box2.Product.Name);
             
+        }
+    }
+
+    class Logger
+    {
+        public void Log(Product product)
+        {
+            Console.WriteLine("Product name is {0}, and it's produced at {1}. It's Price is {2}", product.Name, DateTime.UtcNow, product.Price);
         }
     }
 
     class Product
     {
         public string Name { get; set; }
+        public double Price { get; set; }
     }
 
     class Box
@@ -30,10 +42,14 @@
 
     class WrapFactory
     {
-        public Box WrapProduct(Func<Product> getProduct)
+        public Box WrapProduct(Func<Product> getProduct, Action<Product> logCallback)
         {
             Box box = new Box();
             Product product = getProduct.Invoke();
+            if (product.Price > 50)
+            {
+                logCallback(product);
+            }
             box.Product = product;
             return box;
         }
@@ -45,6 +61,7 @@
         {
             Product product = new Product();
             product.Name = "Pizza";
+            product.Price = 10;
             return product;
         }
 
@@ -52,6 +69,7 @@
         {
             Product product= new Product();
             product.Name = "Toy Car";
+            product.Price=100;
             return product ;
         }
     }
