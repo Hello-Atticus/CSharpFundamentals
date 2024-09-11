c9ab2f1d6bbdc0d8a1aade189bba364573461ea0
Atticus Finch <nineday9527@outlook.com>
2024-09-12 00:48:15 +1200

    Event example2: 事件响应者和事件拥有者相同

---
+++ EventExample2/EventExample2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Forms;
+namespace EventExample2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //事件的拥有者和事件的响应者是一个，即form
+            MyForm form = new MyForm();
+            form.Click += form.Clicked;
+            form.ShowDialog();
+            
+        }
+    }
+
+    class MyForm : Form
+    {
+        internal void Clicked(object? sender, EventArgs e)
+        {
+            this.Text = DateTime.Now.ToString();
+        }
+    }
+}
