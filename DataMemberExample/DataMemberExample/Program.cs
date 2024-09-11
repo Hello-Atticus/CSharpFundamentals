c3c76d6999cf7a164ecbc8cdfbf2725f5776512e
Atticus Finch <nineday9527@outlook.com>
2024-09-12 11:09:22 +1200

    Data member example: 字段

---
+++ DataMemberExample/DataMemberExample/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace DataMemberExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            int stuAge = student.Age = 10;
+            Console.WriteLine(stuAge);
+        }
+    }
+
+    class Student
+    {
+        public int Age;
+        public int Score;
+
+        public Student()
+        {
+            this.Age = 20;
+        }
+
+
+
+    }
+}
