c4144ce22949cbcba3a12ae4ccd4115e29f61c17
Atticus Finch <nineday9527@outlook.com>
2024-09-13 11:17:05 +1200

    HelloClass: Abstract

---
+++ HelloClassAbstract/HelloClassAbstract/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloClassAbstract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle v1 = new Car();
+            v1.Run();
+
+            Vehicle v2 = new Truck();
+            v2.Run();
+        }
+    }
+
+    abstract class Vehicle
+    {
+        public void Stop()
+        {
+            Console.WriteLine("Stopped.");
+        }
+
+        public void Fill()
+        {
+            Console.WriteLine("Fill petrol..");
+        }
+
+        public abstract void Run();
+    }
+
+    class Car:Vehicle
+    {
+        public override void Run()
+        {
+            Console.WriteLine("Car is running...");
+        }
+    }
+    class Truck : Vehicle
+    {
+        public override void Run()
+        {
+            Console.WriteLine("Truck is running...");
+        }
+    }
+    class RaceCar : Vehicle
+    {
+        public override void Run()
+        {
+            Console.WriteLine("RaceCar is running...");
+        }
+    }
+}
