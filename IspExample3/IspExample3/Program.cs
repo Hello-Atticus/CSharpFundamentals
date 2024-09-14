1d336e95795b55dbaf800d1c6586bca402edbff4
Atticus Finch <nineday9527@outlook.com>
2024-09-14 12:10:21 +1200

    IspExample3：接口显示调用

---
+++ IspExample3/IspExample3/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace IspExample3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //只有用IKiller定义的变量才能访问kill方法
+            IKiller killer = new WarmKiller();
+            killer.Kill();
+            var wk = (IGentleman)killer;
+            wk.Love();
+        }
+    }
+
+    interface IKiller
+    {
+        void Kill();
+    }
+    interface IGentleman
+    {
+        void Love();
+    }
+
+    class WarmKiller : IGentleman, IKiller
+    {
+        public void Love()
+        {
+            Console.WriteLine("I love you!");
+        }
+
+        void IKiller.Kill()
+        {
+            Console.WriteLine("I will kill the enamy");
+        }
+    }
+}
