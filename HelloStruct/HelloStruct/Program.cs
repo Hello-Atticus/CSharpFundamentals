9982d5b7a292115d933ece449430f0be8023753f
Atticus Finch <nineday9527@outlook.com>
2024-09-15 19:01:38 +1200

    Hello Struct example

---
+++ HelloStruct/HelloStruct/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace HelloStruct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.ID = 1;
+            student.Name = "Test";
+            student.Report();
+
+            //struct是值类型
+            Student student2 = student;
+            student2.ID = 2;
+            student2.Name = "Test2";
+            student2.Report();
+
+        }
+    }
+
+    interface IStudent
+    {
+        void Report();
+    }
+    //struct可以使用接口
+    struct Student : IStudent
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public void Report()
+        {
+            Console.WriteLine($"I'm #{ID} student {Name}");
+        }
+    }
+}
